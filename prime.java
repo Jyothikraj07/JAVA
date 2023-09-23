c145d48320d3af7467d55dc881b02809ddab1b1e
Jyothikraj07 <132121522+Jyothikraj07@users.noreply.github.com>
2023-09-23 11:45:14 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+public class Main {
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a number to check whether it is a prime number or not");
+
+    int num = sc.nextInt();
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
